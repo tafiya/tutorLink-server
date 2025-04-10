1d5236480616b6208aff44643bfef841440f855d
tafiya <tafiapinkey@gmail.com>
2025-04-11 01:50:21 +0600

    build

--- dist/app/modules/reviews/review.model.js
+++ dist/app/modules/reviews/review.model.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = require("mongoose");
 const ReviewSchema = new mongoose_1.Schema({
     tutorId: { type: String, required: true },
+    name: { type: String, required: true },
     rating: { type: Number, required: true },
     reviewText: { type: String, required: true },
 }, {
